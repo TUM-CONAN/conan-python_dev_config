5bddbd22eb478c53f1181248b49523429a6bed36
Ulrich Eck <ulrich.eck@tum.de>
2024-12-15 20:37:43 +0100

    build with conan-center python correctly

--- conanfile.py
+++ conanfile.py
@@ -36,16 +36,13 @@ class PythonDevConfigConan(ConanFile):
         "with_system_python": False,
     }
 
-    def build_requirements(self):
-        if not self.options.with_system_python:
-            self.build_requires("cpython/[~{}]".format(self.options.python_version))
-
     def requirements(self):
         if not self.options.with_system_python:
-            self.requires("python-pip/24.3.1@camposs/stable")
-            self.requires("python-setuptools/75.6.0@camposs/stable")
-            self.requires("cython/3.0.11-1@camposs/stable")
-            self.requires("python-numpy/2.2.0@camposs/stable")
+            self.requires("cpython/[~{}]".format(self.options.python_version), run=True)
+            self.requires("python-pip/24.3.1@camposs/stable", run=True)
+            self.requires("python-setuptools/75.6.0@camposs/stable", run=True)
+            self.requires("cython/3.0.11-1@camposs/stable", run=True)
+            self.requires("python-numpy/2.2.0@camposs/stable", run=True)
 
     def package_id(self):
         self.info.clear()
@@ -58,17 +55,29 @@ class PythonDevConfigConan(ConanFile):
         if not self.have_python_dev:
             raise RuntimeError("Python development environment not correctly setup.")
 
-        self.cpp_info.includedirs = [self._python_include_dir]
-        self.cpp_info.libdirs = [os.path.dirname(self._python_lib)]
-        self.cpp_info.libs = [self._python_lib_ldname]
-        self.cpp_info.bindirs = [os.path.dirname(self._python_lib), os.path.dirname(self._python_exec)]
+        if self.options.with_system_python:
+            self.cpp_info.includedirs = [self._python_include_dir]
+            self.cpp_info.libdirs = [os.path.dirname(self._python_lib)]
+            self.cpp_info.libs = [self._python_lib_ldname]
+            self.cpp_info.bindirs = [os.path.dirname(self._python_lib), os.path.dirname(self._python_exec)]
 
-        python_home = self._python_prefix
-        self.buildenv_info.append_path("PYTHONPATH", os.path.join(python_home, "lib", "python%s" % self._python_version))
-        self.buildenv_info.define("PYTHONHOME", python_home)
-        self.buildenv_info.append_path("PATH", os.path.dirname(self._python_lib))
-        self.buildenv_info.append_path("PATH", os.path.dirname(self._python_exec))
-        self.buildenv_info.append_path("LD_LIBRARY_PATH", os.path.join(self._python_lib))
+            python_home = self._python_prefix
+            self.buildenv_info.append_path("PYTHONPATH", os.path.join(python_home, "lib", "python%s" % self._python_version))
+            self.buildenv_info.define("PYTHONHOME", python_home)
+            self.buildenv_info.append_path("PATH", os.path.dirname(self._python_lib))
+            self.buildenv_info.append_path("PATH", os.path.dirname(self._python_exec))
+            self.buildenv_info.append_path("LD_LIBRARY_PATH", os.path.join(self._python_lib))
+        else:
+            cpy_dep = self.dependencies["cpython"]
+            self.cpp_info.includedirs = cpy_dep.cpp_info.includedirs
+            self.cpp_info.libdirs = cpy_dep.cpp_info.libdirs
+            self.cpp_info.libs = cpy_dep.cpp_info.libs
+            self.cpp_info.bindirs = cpy_dep.cpp_info.bindirs
+
+            self.buildenv_info.append_path("PTYONHOME", cpy_dep.package_folder)
+            self.runenv_info.append_path("PTYONHOME", cpy_dep.package_folder)
+
+
 
         self.conf_info.define("tools.python_dev_config:python_version", self._python_version)
         self.conf_info.define_path("tools.python_dev_config:python", self._python_exec)
