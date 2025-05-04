4eadfac30b240d1b8204ef63867915f637ba136e
Ulrich Eck <ulrich.eck@tum.de>
2025-05-04 11:56:49 +0200

    bumped numpy version for custom-python

--- conanfile.py
+++ conanfile.py
@@ -12,7 +12,7 @@ class PythonDevConfigConan(ConanFile):
     python_requires_extend = "camp_common.CampPythonBase"
 
     name = "python_dev_config"
-    upstream_version = '1.1'
+    upstream_version = '1.2'
     package_revision = ''
     version = "{0}{1}".format(upstream_version, package_revision)
 
@@ -30,7 +30,7 @@ class PythonDevConfigConan(ConanFile):
             self.requires("python-pip/24.3.1@camposs/stable", run=True)
             self.requires("python-setuptools/75.6.0@camposs/stable", run=True)
             self.requires("cython/3.0.11-1@camposs/stable", run=True)
-            self.requires("python-numpy/1.26.4@camposs/stable", run=True)
+            self.requires("python-numpy/2.2.0@camposs/stable", run=True)
 
     def package_id(self):
         self.info.clear()
